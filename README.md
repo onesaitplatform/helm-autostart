74cd4fdab6f9ce071621cc750383cde414222df4
jriveiro <jriveiro@minsait.com>
2020-09-10 12:22:15 +0200

    commit

--- README.md
+++ README.md
@@ -11,13 +11,13 @@ Requirements:
 ### Plugin installation
 
 ```
-> helm plugin install https://github.com/onesaitplatform/helm-certgen.git
+> helm plugin install https://cicd.onesaitplatform.com/gitlab/onesait-platform/helm-autostart.git
 ```
 
 ### Uninstall
 
 ```
-> helm plugin uninstall certgen
+> helm plugin uninstall autostart
 ```
 
 ### List plugins
@@ -29,38 +29,11 @@ Requirements:
 ### Plugin usage:
 
 - Available flags:
-  - domain: proper domain or the wildcard to generate certificate
-  - host: name of the dns that is going to use in the Openshift route
+  - namesapce: namespace which contents the OP deployments
 
 ```
-> helm certgen --domain *.example.com --host onesaitplatform.example.com
+> helm autostart --namespace <op_namespace>
 ```
 
-Once the plugin execution is finished, it generates two folders in the same directory that the plugin had been executed:
+Once the plugin execution is finished, the platform pods will be scaled.
 
-- ssl: it includes all the files necessary to generate self signed certificates (CA, server certificate, private key...)
-- route-template: it includes **Route.yml** Openshift manifest file that had been deployed to an existing cluster
-
-```
-apiVersion: route.openshift.io/v1
-kind: Route
-metadata:
-  name: loadbalancer-route
-spec:
-  host: testing.apps.private-cluster.es
-  path: /
-  to:
-    kind: Service
-    name: loadbalancer
-  port:
-    targetPort: http
-  tls:
-    termination: edge
-    key: |
-      -----BEGIN RSA PRIVATE KEY-----
-      MIIEp...
-    certificate: |
-      -----BEGIN CERTIFICATE-----
-      MIIDf...
-
-```
---
+++ autostart.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# if an error occurs the script stops inmediately
+set -e
+
+help() {
+  echo
+  echo "Plugin usage:"
+  echo
+  echo "helm autostart --namespace onesait-platform"
+  echo
+}
+
+parseParams() {
+
+  if [[ ${#params[@]} -lt 2 ]]; then
+    echo "Bad number of params!"
+    help
+    exit 1
+  fi
+
+  if [[ ${params[0]} != '--namespace' ]]; then
+    echo "Bad parameter! --namespace"
+    help
+    exit 1
+  fi
+
+  namespace=${params[1]}
+
+}
+
+# Load configuration file
+source $HELM_PLUGIN_DIR/config.properties
+
+echo "K8s Namespace: "$namespace
+
+if [[ ! -d  $(pwd)/ssl ]]; then
+  mkdir $(pwd)/ssl
+fi
+
+params=("$@")
+
+parseParams
+
+echo $namespace
+                     
+echo "[$(date)] Starting quasar deployment" >> /var/log/onesait.log
+echo "Desplegando cosas"
+#kubectl scale deployment quasar --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 10s
+
+#echo "[$(date)] Starting Cache service" >> /var/log/onesait.log
+#kubectl scale deployment cacheservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+    
+#sleep 30s  
+      
+#echo "[$(date)] Starting Control Panel deployment" >> /var/log/onesait.log
+#kubectl scale deployment controlpanelservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+    
+#sleep 400s 
+     
+#echo "[$(date)] Starting Semantic Information Broker deployment" >> /var/log/onesait.log
+#kubectl scale deployment routerservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+          
+#echo "[$(date)] Starting IoT broker deployment" >> /var/log/onesait.log
+#kubectl scale deployment iotbrokerservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+      
+#sleep 60s
+            
+#echo "[$(date)] Starting OAuth deployment" >> /var/log/onesait.log
+#kubectl scale deployment oauthservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s    
+      
+#echo "[$(date)] Starting API manager deployment" >> /var/log/onesait.log
+#kubectl scale deployment apimanagerservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+      
+#echo "[$(date)] Starting DashBoard Engine deployment" >> /var/log/onesait.log
+#kubectl scale deployment dashboardengineservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+          
+#echo "[$(date)] Starting RTDB Maintainer deployment" >> /var/log/onesait.log
+#kubectl scale deployment rtdbmaintainerservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+          
+#echo "[$(date)] Starting Device Simulator deployment" >> /var/log/onesait.log
+#kubectl scale deployment devicesimulator --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+          
+#echo "[$(date)] Starting Monitoring UI deployment" >> /var/log/onesait.log
+#kubectl scale deployment monitoringuiservice --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+    
+#echo "[$(date)] Starting Notebooks deployment" >> /var/log/onesait.log
+#kubectl scale deployment zeppelin --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+ 
+#sleep 60s    
+
+#echo "[$(date)] Starting Streamsets deployment" >> /var/log/onesait.log
+#kubectl scale deployment streamsets --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+
+#echo "[$(date)] Starting Streamsets-secondary deployment" >> /var/log/onesait.log
+#kubectl scale deployment streamsets-secondary --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
+
+#sleep 60s
+      
+#echo "[$(date)] Starting Load Balancer deployment" >> /var/log/onesait.log
+#kubectl scale deployment loadbalancer --namespace=$domain --replicas=1 2>&1 >> /var/log/onesait.log
